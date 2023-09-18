fbd32d83c76502e98d08b44df1d077158796b4c0
Denis <grabko.denis11@gmail.com>
2023-09-18 12:38:59 +0300

    first-without-display-of-card

--- src/js/find-photo-api.js
+++ src/js/find-photo-api.js
@@ -1,52 +1,34 @@
+// Ваш файл find-photo-api.js
 import axios from 'axios';
 import notiflix from "notiflix"
 
 const apiKey = '39444831-9a9227c6cf2b75d1cfcf35b46';
-const searchQuery = 'dogs';
 
-//axios.defaults.headers.common['x-api-key'] = 'live_lJcJU1AuNnhlcVjceNk6Xjrh8ITLhUvkZcjR4Semjtm4rRofZYSDP6yaWhF3phbO';
+const apiURL = 'https://pixabay.com/api/';
 
- 
-const apiURL ='https://pixabay.com/api/';
-
-export function fetchItems() {
-  axios.get(`${apiURL}`, {
+export function fetchItemsByTag(tag) {
+  axios.get(apiURL, {
     params: {
         key: apiKey,
-        q: searchQuery,
+        q: tag, // Використовуємо параметр tag для пошуку за тегом.
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-    }})
-.then((response) => {
-    console.log(response.data);
-})
-.catch((error) => {
+    }
+  })
+  .then((response) => {
+    const images = response.data.hits;
+    // Тут ви можете використовувати знайдені зображення для подальших дій.
+    return images;
+  })
+  .catch((error) => {
     console.error('Помилка запиту до Pixabay API:', error);
-    notiflix.Notify.failure('Помилка запиту до Pixabay API');
-})
+    const errorMessage = notiflix.Notify.failure('Помилка запиту до Pixabay API');
+    return errorMessage;
+  });
 }
 
 
-export function fetchItemsById(TypeOfItemName) {
-    axios.get(`${apiURL}/${TypeOfItemName}`, {
-        params: {
-           key: apiKey,
-           q: searchQuery,
-           image_type: 'photo',
-           orientation: 'horizontal',
-           safesearch: true,
-        }
-    })
-    .then((result) => {
-        console.log(response.data);
-    })
-    .catch((error) => {
-        console.error('Помилка запиту до Pixabay API:', error);
-        notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    })
-}
-
-return fetchItems();
+return fetchItemsByTag();
 //const text = notiflix.Notify.failure("Please choose a date in the future"); return text;
     
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,98 @@
-import { fetchItems, fetchItemsById } from 'find-photo-api.js';
+import { fetchItemsByTag } from './find-photo-api.js';
 
-fetchItems();
-fetchItemsById();
+const form = document.getElementById('search-form');
+const input = form.querySelector('input[name="searchQuery"]');
+const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+let page = 1;
+let searchQuery = '';
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    searchQuery = input.value.trim();
+    page = 1; 
+
+    if (searchQuery) {
+        try {
+            const images = await fetchItemsByTag(searchQuery, page);
+            displayImages(images);
+            toggleLoadMoreButton(images);
+        } catch (error) {
+            console.error('Error fetching images:', error);
+        }
+    }
+});
+
+loadMoreButton.addEventListener('click', async () => {
+    try {
+        page++;
+        const images = await fetchItemsByTag(searchQuery);
+        displayImages(images);
+        toggleLoadMoreButton(images);
+    } catch (error) {
+        console.error('Error fetching more images:', error);
+    }
+});
+
+function displayImages(images) {
+    if (page === 1) {
+        gallery.innerHTML = ''; 
+    }
+
+    images.forEach((image) => {
+        const cardInfo = `
+        <div class="photo-card">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy">
+          <div class="info">
+            <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+            <p class="info-item"><b>Views:</b> ${image.views}</p>
+            <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+            <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+          </div>
+        </div>
+        `;
+        gallery.innerHTML += cardInfo;
+    });
+}
+
+function toggleLoadMoreButton(images) {
+    if (images.length <= 40) {
+        loadMoreButton.style.display = 'none'; 
+        gallery.insertAdjacentHTML('beforeend', '<p class="end-message">We\'re sorry, but you\'ve reached the end of search results.</p>');
+    } else {
+        loadMoreButton.style.display = 'block'; 
+        const endMessage = gallery.querySelector('.end-message');
+        if (endMessage) {
+            endMessage.remove(); 
+        }
+    }
+}
+
+form.submit();
+return form;
+
+
+
+// const imageObj = {
+//     webformatURL: images.webformatURL,
+//     largeImageURL: images.largeImageURL,
+//     tags: images.tags,
+//     likes: images.likes,
+//     views: images.views,
+//     comments: images.comments,
+//     downloads: images.downloads,
+// };
+
+// webformatURL - посилання на маленьке зображення для списку карток.
+//  - посилання на велике зображення.
+//  - рядок з описом зображення. Підійде для атрибуту alt.
+//  - кількість лайків.
+// - кількість переглядів.
+//  - кількість коментарів.
+//  - кількість завантажень. 
+
+//const imgElement = document.createElement('img');
+        // imgElement.src = image.webformatURL;
+        // imgElement.alt = image.tags;
+        // imageContainer.appendChild(imgElement);
