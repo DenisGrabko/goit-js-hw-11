b9e6eda9c882fe0cd8e4e0743983913b6a7f14b4
Denis <grabko.denis11@gmail.com>
2023-09-19 16:17:57 +0300

    add-pagination-with-little-style

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,76 @@
-// Ваш файл script.js
 import { fetchItemsByTag } from './find-photo-api.js';
 
 const form = document.getElementById('search-form');
 const input = form.querySelector('input[name="searchQuery"]');
 const imageContainer = document.querySelector('.photo-card');
+const loadMoreButton = document.querySelector('.load-more');
+
+let page = 1;
+let searchQuery = '';
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const searchQuery = input.value.trim();
+    searchQuery = input.value.trim();
+    page = 1;
 
     if (searchQuery) {
         try {
-            const images = await fetchItemsByTag(searchQuery);
-
-            if (images.length === 0) {
-                showNoImagesMessage();
-            } else {
-                displayImages(images);
-            }
+            const images = await fetchItemsByTag(searchQuery, page);
+            displayImages(images);
+            toggleLoadMoreButton(images);
         } catch (error) {
             console.error('Error fetching images:', error);
-            // Handle the error (e.g., show an error message).
         }
     }
 });
 
-function showNoImagesMessage() {
-    imageContainer.innerHTML = '<p class="no-images-message">Sorry, there are no images matching your search query. Please try again.</p>';
-}
+loadMoreButton.addEventListener('click', async () => {
+    try {
+        page++;
+        const images = await fetchItemsByTag(searchQuery, page);
+        displayImages(images);
+        toggleLoadMoreButton(images);
+    } catch (error) {
+        console.error('Error fetching more images:', error);
+    }
+});
 
 function displayImages(images) {
-    imageContainer.innerHTML = '';
+    if (page === 1) {
+        imageContainer.innerHTML = ''; // Очистити контейнер при першому запиті
+    }
 
-    images.forEach((image) => {       
+    images.forEach((image) => {
         const cardInfo = `
         <div class="photo-one-card">
-          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="250px" height="150px">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="450px" height="250px">
           <div class="info">
-            <p class="info-item">${image.likes}</p>
-            <p class="info-item">${image.views}</p>
-            <p class="info-item">${image.comments}</p>
-            <p class="info-item">${image.downloads}</p>
+            <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+            <p class="info-item"><b>Views:</b> ${image.views}</p>
+            <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+            <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
           </div>
         </div>
-        `       
+        `;
+
         imageContainer.innerHTML += cardInfo;
     });
+}
+
+function toggleLoadMoreButton(images) {
+    if (images.length < 40) {
+        loadMoreButton.style.display = 'none'; // Ховати кнопку, якщо немає більше зображень
+        if (page > 1) {
+            imageContainer.insertAdjacentHTML('beforeend', '<p class="end-message">We\'re sorry, but you\'ve reached the end of search results.</p>');
+        }
+    } else {
+        loadMoreButton.style.display = 'block'; // Показувати кнопку, якщо є більше зображень
+        const endMessage = imageContainer.querySelector('.end-message');
+        if (endMessage) {
+            endMessage.remove(); // Видалити повідомлення про завершення пошуку, якщо воно є
+        }
+    }
 }
 
 
